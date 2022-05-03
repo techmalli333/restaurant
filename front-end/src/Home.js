95ded050fad22f9611ac5c08c505b72bd0dfaf25
techmalli333 <techmalli333@gmail.com>
2022-05-04 02:11:22 +0530

    food api data  added

--- front-end/src/Home.js
+++ front-end/src/Home.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './index.css'
-
+import React from "react";
+import { Link } from "react-router-dom";
+import "./index.css";
+const api = require("./data/fooodApi.json").items;
 
 const Home = () => {
-  const user = 'John'
+  const user = "John";
   return (
     <div>
       <center>
         <h3>Welcome to Home page</h3>
-        <Link to={`/dashboard?name=${user}&age=${26}`} className='Link'>Dashboard</Link>
-        <Link to='/about' className='Link'>About</Link>
+        <Link to={`/dashboard?name=${user}&age=${26}`} className="Link">
+          Dashboard
+        </Link>
+        <Link to="/about" className="Link">
+          About
+        </Link>
+        {api.map((each) => (
+          <div>
+            <p>{each.name}</p>
+            <img src={"/img/" + each.url} />
+          </div>
+        ))}
       </center>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
